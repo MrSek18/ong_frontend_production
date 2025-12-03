769f9dfbbe5fe1e35d198b704e3761b700123561
kevin <mrsek2022@gmail.com>
2025-12-03 00:34:46 -0500

    avance

--- src/components/Donacion.jsx
+++ src/components/Donacion.jsx
@@ -1,6 +1,13 @@
 
 import Header from "../components/Header.jsx";
 import DonacionImg from "../assets/imgsec2QuienesSomos.png";
+import formAmericanExpress from "../assets/formAmericanExpress.png";
+import formDinersClub from "../assets/formDinersClubLogo.png";
+import formMastercard from "../assets/formMastercardLogo.png";
+import formPsi from "../assets/formPsiLogo.png";
+import formUnionPay from "../assets/formUnionPay.png";
+import formVisa from "../assets/formVisaLogo.png";
+import logoAlientaOriginal from "../assets/alientaPeruanoLogoOriginal.png";
 import { useState } from "react";
 
 export default function QuienesSomos() {
@@ -10,6 +17,15 @@ export default function QuienesSomos() {
   const [monto, setMonto] = useState(null);     
   const [otroMonto, setOtroMonto] = useState("");
   const [errors, setErrors] = useState({ plan: "", monto: "" });
+  const [showForm, setShowForm] = useState(false);
+
+  const [tarjeta, setTarjeta] = useState("");
+  const [expiracion, setExpiracion] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [email, setEmail] = useState("");
+  const [movil, setMovil] = useState("");
+  const [suscripcion, setSuscripcion] = useState(true);
+  const [montoMaximo, setMontoMaximo] = useState(false);
 
   const handleNext = () => {
     let newErrors = { plan: "", monto: "" };
@@ -27,9 +43,14 @@ export default function QuienesSomos() {
 
     // Si no hay errores, continuar
     if (!newErrors.plan && !newErrors.monto) {
-      const aporteFinal = monto === "otro" ? otroMonto : monto;
-      alert(`Plan: ${plan}, Monto: ${aporteFinal}`);
+      setShowForm(true)
     }
+
+    
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Procesando pago de S/ 30.00 con tarjeta ${tarjeta}`);
   };
   return (
     <div className="w-full h-auto overflow-x-hidden ">
@@ -53,108 +74,239 @@ export default function QuienesSomos() {
                 }}
                 ></div>
     
-                <div className="flex flex-col w-full lg:w-[40%] h-auto sm:h-screen text-xl order-1 lg:order-2 text-[#231f20] justify-center items-center  gap-y-6 bg-blue-950 text-white relative pt-50 pb-10 sm:pt-30 sm:pb-0">
+                <div className="flex flex-col w-full lg:w-[40%] h-auto lg:h-screen text-xl order-1 lg:order-2 text-[#231f20] justify-center items-center  gap-y-6 bg-blue-950 text-white relative pt-50 pb-10 lg:pt-30 lg:pb-0">
     
-                {/* first contenedor */}
-                {!showReactive && (
-                    <div className="flex flex-col w-[85%] gap-6 items-center justify-center ">
-                    <h1 className="text-5xl ">¡Con tu ayuda</h1>
-                    <h2 className="text-4xl ">podemos ayudarlos!</h2>
+                    {/* first contenedor */}
+                    {!showReactive && !showForm && (
+                        <div className="flex flex-col w-[85%] gap-6 items-center justify-center ">
+                        <h1 className="text-5xl ">¡Con tu ayuda</h1>
+                        <h2 className="text-4xl ">podemos ayudarlos!</h2>
 
-                    <p className="text-2xl w-[80%]">
-                        Tu apoyo puede convertir a niños con talento y pocos recursos en futbolistas profesionales y líderes positivos
-                    </p>
-                    <button
-                        onClick={() => setShowReactive(true)}
-                        className="bg-red-600 text-white px-6 py-4 rounded-full cursor-pointer hover:scale-110 transform-gpu transition text-2xl"
-                    >
-                        Dona ahora
-                    </button>
-                    </div>
-                )}
-
-                {/* reactive contenedor */}
-                {showReactive && (
-                <div className="flex flex-col w-[95%] lg:w-[70%] gap-6 items-center justify-center bg-[#a7a9ac] text-[#231f20] xl-p-10 p-6 relative">
-                    
-                    {/* botón de cierre */}
-                    <button
-                    onClick={() => setShowReactive(false)}
-                    className="absolute top-2 right-4 text-black text-xl font-bold"
-                    >
-                    ✕
-                    </button>
-
-                    <h1 className="text-2xl xl:text-3xl">DONA AHORA</h1>
-                    <h2 className="text-base xl:text-lg">
-                    Con tu apoyo ayudas al desarrollo personal y profesional a niños vulnerables con talento. <br />
-                    Elije el monto.
-                    </h2>
-
-                    {/* Planes */}
-                    <div className="flex flex-row w-full text-base xl:text-lg">
-                    <button
-                        onClick={() => setPlan("mensual")}
-                        className={`p-3 w-1/2 ${plan === "mensual" ? "bg-[#fff200]" : "bg-white"}`}
-                    >
-                        MENSUAL
-                    </button>
-                    <button
-                        onClick={() => setPlan("unica")}
-                        className={`p-3 w-1/2 ${plan === "unica" ? "bg-[#fff200]" : "bg-white"}`}
-                    >
-                        ÚNICA
-                    </button>
-                    </div>
-                    {errors.plan && <p className="text-red-600 text-sm">{errors.plan}</p>}
-
-                    {/* Montos */}
-                    <div className="flex flex-row w-full text-xs sm:text-lg lg:text-xs xl:text-lg">
-                    {["30", "60", "90", "otro"].map((valor, i) => (
+                        <p className="text-2xl w-[80%]">
+                            Tu apoyo puede convertir a niños con talento y pocos recursos en futbolistas profesionales y líderes positivos
+                        </p>
                         <button
-                        key={valor}
-                        onClick={() => setMonto(valor)}
-                        className={`p-3 w-1/4 border-r border-black ${
-                            monto === valor ? "bg-[#fff200]" : "bg-white"
-                        } ${i === 3 ? "border-r-0" : ""}`}
+                            onClick={() => setShowReactive(true)}
+                            className="bg-red-600 text-white px-6 py-4 rounded-full cursor-pointer hover:scale-110 transform-gpu transition text-2xl"
                         >
-                        {valor === "otro" ? "OTRO" : `S/. ${valor}`}
+                            Dona ahora
                         </button>
-                    ))}
-                    </div>
+                        </div>
+                    )}
 
-                    {/* Input para "OTRO" solo si está seleccionado */}
-                    {monto === "otro" && (
-                    <div className="w-full flex flex-col gap-2">
-                        <label className="text-sm font-semibold">Ingresa tu monto:</label>
-                        <input
-                        type="number"
-                        value={otroMonto}
-                        onChange={(e) => setOtroMonto(e.target.value)}
-                        className="border border-black p-2 rounded"
-                        placeholder="Ejemplo: 120"
-                        />
+                    {/* reactive contenedor */}
+                    {showReactive && !showForm && (
+                    <div className="flex flex-col w-[95%] lg:w-[70%] gap-6 items-center justify-start bg-[#a7a9ac] text-[#231f20] xl-p-10 p-6 relative overflow-y-auto lg:max-h-110 ">
+                        {/* flex flex-col w-[75%]  lg:w-[70%] gap-6 items-center justify-start bg-white text-[#231f20] p-6 relative rounded overflow-y-auto max-h-100 */}
+                        {/* botón de cierre */}
+                        <button
+                        onClick={() => setShowReactive(false)}
+                        className="absolute top-2 right-4 text-black text-xl font-bold"
+                        >
+                        ✕
+                        </button>
+
+                        <h1 className="text-2xl xl:text-3xl">DONA AHORA</h1>
+                        <h2 className="text-base xl:text-lg">
+                        Con tu apoyo ayudas al desarrollo personal y profesional a niños vulnerables con talento. <br />
+                        Elije el monto.
+                        </h2>
+
+                        {/* Planes */}
+                        <div className="flex flex-row w-full text-base xl:text-lg">
+                        <button
+                            onClick={() => setPlan("mensual")}
+                            className={`p-3 w-1/2 ${plan === "mensual" ? "bg-[#fff200]" : "bg-white"}`}
+                        >
+                            MENSUAL
+                        </button>
+                        <button
+                            onClick={() => setPlan("unica")}
+                            className={`p-3 w-1/2 ${plan === "unica" ? "bg-[#fff200]" : "bg-white"}`}
+                        >
+                            ÚNICA
+                        </button>
+                        </div>
+                        {errors.plan && <p className="text-red-600 text-sm">{errors.plan}</p>}
+
+                        {/* Montos */}
+                        <div className="flex flex-row w-full text-xs sm:text-lg lg:text-xs xl:text-lg">
+                        {["30", "60", "90", "otro"].map((valor, i) => (
+                            <button
+                            key={valor}
+                            onClick={() => setMonto(valor)}
+                            className={`p-3 w-1/4 border-r border-black ${
+                                monto === valor ? "bg-[#fff200]" : "bg-white"
+                            } ${i === 3 ? "border-r-0" : ""}`}
+                            >
+                            {valor === "otro" ? "OTRO" : `S/. ${valor}`}
+                            </button>
+                        ))}
+                        </div>
+
+                        {/* Input para "OTRO" solo si está seleccionado */}
+                        {monto === "otro" && (
+                        <div className="w-full flex flex-col gap-2">
+                            <label className="text-sm font-semibold">Ingresa tu monto:</label>
+                            <input
+                            type="number"
+                            value={otroMonto}
+                            onChange={(e) => setOtroMonto(e.target.value)}
+                            className="border border-black p-2 rounded"
+                            placeholder="Ejemplo: 120"
+                            />
+                        </div>
+                        )}
+                        {errors.monto && <p className="text-red-600 text-sm">{errors.monto}</p>}
+
+                        {/* Botón siguiente */}
+                        <div className="w-full text-xl text-center">
+                        <button
+                            onClick={handleNext}
+                            className="text-center p-2 bg-[#ed1c24] text-white w-full 
+                                    transform transition duration-300 
+                                    hover:scale-105 active:scale-95"
+                        >
+                            SIGUIENTE
+                        </button>
+                        </div>
+
+                        <p className="text-sm w-full">
+                        Tu donación es deducible de impuestos.
+                        </p>
                     </div>
                     )}
-                    {errors.monto && <p className="text-red-600 text-sm">{errors.monto}</p>}
 
-                    {/* Botón siguiente */}
-                    <div className="w-full text-xl text-center">
-                    <button
-                        onClick={handleNext}
-                        className="text-center p-2 bg-[#ed1c24] text-white w-full 
-                                transform transition duration-300 
-                                hover:scale-105 active:scale-95"
-                    >
-                        SIGUIENTE
-                    </button>
+                    {/* Vista del formulario de tarjeta */}
+                    {showForm && (
+                    <div className="flex flex-col w-[75%]  lg:w-[70%] gap-6 items-center justify-start bg-white text-[#231f20] p-6 relative rounded overflow-y-auto max-h-100">
+                        {/* Botón de cierre */}
+                        <button
+                        type="button"
+                        onClick={() => setShowForm(false)}
+                        className="absolute top-2 right-4 text-black text-xl font-bold"
+                        >
+                        ✕
+                        </button>
+
+                        <img src={logoAlientaOriginal} className="bg-[#a7a9ac] px-10 py-3 mt-5"/>
+                        <p className="text-sm mb-2 text-center text-gray-600 font-sans font-medium">
+                        Recuerda activar las compras por internet con tu banco
+                        </p>
+
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
+                        <input
+                            type="text"
+                            placeholder="Número de Tarjeta"
+                            value={tarjeta}
+                            onChange={(e) => setTarjeta(e.target.value)}
+                            className="border p-2 rounded font-sans font-medium"
+                            required
+                        />
+                        <div className="flex gap-4">
+                            <input
+                            type="text"
+                            placeholder="MM/AA"
+                            value={expiracion}
+                            onChange={(e) => setExpiracion(e.target.value)}
+                            className="border p-2 rounded w-1/2 font-sans font-medium"
+                            required
+                            />
+                            <input
+                            type="text"
+                            placeholder="CVV"
+                            value={cvv}
+                            onChange={(e) => setCvv(e.target.value)}
+                            className="border p-2 rounded w-1/2 font-sans font-medium"
+                            required
+                            />
+                        </div>
+
+                        <div className="flex gap-4">
+                            <input
+                            type="text"
+                            placeholder="Nombre titular"
+                            value={expiracion}
+                            onChange={(e) => setExpiracion(e.target.value)}
+                            className="border p-2 rounded w-1/2 font-sans font-medium"
+                            required
+                            />
+                            <input
+                            type="text"
+                            placeholder="Apellido titular"
+                            value={cvv}
+                            onChange={(e) => setCvv(e.target.value)}
+                            className="border p-2 rounded w-1/2 font-sans font-medium"
+                            required
+                            />
+                        </div>
+                        
+                        <input
+                            type="email"
+                            placeholder="Correo electrónico"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="border p-2 rounded font-sans font-medium"
+                            required
+                        />
+
+                        <input
+                            type="text"
+                            placeholder="Móvil"
+                            value={movil}
+                            onChange={(e) => setMovil(e.target.value)}
+                            className="border p-2 rounded font-sans font-medium"
+                            required
+                        />
+
+
+                        <div className="flex flex-col gap-2 text-sm">
+                            <p className="font-semibold">
+                            Pagar ahora: <span className="text-green-700">S/ 30.00</span>
+                            </p>
+                            <label className="flex items-center gap-2">
+                            <input
+                                type="checkbox"
+                                checked={suscripcion}
+                                onChange={() => setSuscripcion(!suscripcion)}
+                            />
+                            Cargo Mensual S/ 30.00
+                            </label>
+                            <label className="flex items-center gap-2">
+                            <input
+                                type="checkbox"
+                                checked={montoMaximo}
+                                onChange={() => setMontoMaximo(!montoMaximo)}
+                            />
+                            Establecer monto máximo
+                            </label>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="bg-red-600 text-white py-2 rounded hover:scale-105 active:scale-95 transition"
+                        >
+                            Suscribirse
+                        </button>
+                        </form>
+
+                        <p className="text-sm mt-4 text-center text-gray-500">
+                        Infórmate sobre el tratamiento de tus datos personales aquí
+                        </p>
+
+                        {/* Logos de seguridad */}
+                        <div className="grid grid-cols-3 mt-4 justify-items-center sm:flex sm:justify-center gap-4">
+                            <img src={formPsi} alt="PCI Security Standards Council" className="h-8 " />
+                            <img src={formVisa} alt="Visa" className="h-8 " />
+                            <img src={formMastercard} alt="Mastercard" className="h-8 " />
+                            <img src={formDinersClub} alt="Diners Club" className="h-8 " />
+                            <img src={formAmericanExpress} alt="American Express" className="h-8 " />
+                            <img src={formUnionPay} alt="UnionPay" className="h-8 " />
+                        </div>
+
                     </div>
+                    )}
 
-                    <p className="text-sm w-full">
-                    Tu donación es deducible de impuestos.
-                    </p>
-                </div>
-                )}
                 </div>
             </div>
         </div>
