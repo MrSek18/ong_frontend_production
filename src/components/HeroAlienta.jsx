6f1a52064eeb8b86702268c9bbf521b22c943b01
kevin <mrsek2022@gmail.com>
2025-12-14 00:33:56 -0500

    avance

--- src/components/HeroAlienta.jsx
+++ src/components/HeroAlienta.jsx
@@ -92,7 +92,7 @@ export default function HeroAlienta() {
                 to="/donaciones" 
                 className="text-md absolute bottom-4 right-4 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-full transition cursor-pointer roboto-condensed"
               >
-                Dona ahora
+                Dona Ahora
               </Link>
 
               
